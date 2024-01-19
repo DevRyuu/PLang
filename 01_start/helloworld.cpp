8810a0bd5eaec7f435df76604581e222d6129bf4
Jasper RYU <jasper.r0328@gmail.com>
2024-01-19 19:36:29 +0900

    2024-01-19

--- 01_start/helloworld.cpp
+++ 01_start/helloworld.cpp
@@ -1,9 +1,18 @@
 // g++ 01_start/helloworld.cpp -o helloworld
 // ./helloworld.exe
 
-#include <iostream>
+#include <iostream> // iostream이라는 헤더를 포함(include)
 
-int main() {
-  std::cout << "Hello, world!" << std::endl;
-  return 0;
+using namespace std; // 네임스페이스 설명 std::cout
+
+int main() // entry point
+{ 
+    cout << "Hello, World!" << endl;
+    // printf("Hello World!!! by printf");
+
+    char user_input[100];
+    cin >> user_input;
+    cout << user_input;
+
+    return 0;
 }
